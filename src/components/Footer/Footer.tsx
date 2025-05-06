22ebcd41c0fae924760e4eafe6ea09f33b145e7b
Matthew Kwon <matthew.o.kwon@gmail.com>
2025-05-06 16:52:11 -0700

    Updated skills section

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,17 +3,17 @@ import { FaGithub } from 'react-icons/fa';
 import './Footer.css';
 
 export default function Footer() {
-  const footerText: string = '  2025 | Matthew Kwon |';
+  const footerText: string = '  2025 | Matthew Kwon';
   return (
     <div className="app-footer">
       {footerText}
-      <a
+      {/* <a
         href="https://github.com/m-kwon"
         target="_blank"
         rel="noopener noreferrer"
       >
         <FaGithub className="footer-gh-link" size="1rem" />
-      </a>
+      </a> */}
     </div>
   );
 }
--- src/components/Skills/SkillList.tsx
+++ src/components/Skills/SkillList.tsx
@@ -3,9 +3,8 @@ import {
   SiCss3,
   SiJavascript,
   SiTypescript,
+  SiPython,
   SiReact,
-  SiFirebase,
-  SiSass,
   SiNodedotjs,
   SiMongodb,
   SiExpress,
@@ -13,10 +12,13 @@ import {
   SiGithub,
   SiJest,
   SiLinux,
-  SiVite,
-  SiPassport,
   SiJsonwebtokens,
+  SiSpringboot,
+  SiMysql,
+  SiPostgresql,
+  SiJunit5
 } from 'react-icons/si';
+import { FaJava } from "react-icons/fa";
 
 const skillList: {
   name: string;
@@ -25,32 +27,20 @@ const skillList: {
 }[] = [
   { name: 'HTML', icon: <SiHtml5 color="#e34f26" />, color: '#e34f26' },
   { name: 'CSS', icon: <SiCss3 color="#264de4" />, color: '#264de4' },
-  {
-    name: 'JavaScript',
-    icon: <SiJavascript color="#f7df1e" />,
-    color: '#f7df1e',
-  },
-  {
-    name: 'TypeScript',
-    icon: <SiTypescript color="#3178c6" />,
-    color: '#3178c6',
-  },
+  { name: 'JavaScript', icon: <SiJavascript color="#f7df1e" />, color: '#f7df1e' },
+  { name: 'TypeScript', icon: <SiTypescript color="#3178c6" />, color: '#3178c6' },
+  { name: 'Java', icon: <FaJava color="#007396" />, color: '#007396' },
+  { name: 'Python', icon: <SiPython color="#4B8BBE" />, color: '#4B8BBE' },
   { name: 'React', icon: <SiReact color="#61dbfb" />, color: '#61dbfb' },
-  {
-    name: 'Firebase',
-    icon: <SiFirebase color="#FFCB2B" />,
-    color: '#FFCB2B',
-  },
-  { name: 'Sass', icon: <SiSass color="#cd669a" />, color: '#cd669a' },
-  { name: 'NodeJS', icon: <SiNodedotjs color="#68a063" />, color: '#68a063' },
-  { name: 'MongoDB', icon: <SiMongodb color="#589636" />, color: '#589636' },
-  { name: 'Express', icon: <SiExpress color="#589636" />, color: '#589636' },
+  { name: 'Spring Boot', icon: <SiSpringboot color="#6DB33F" />, color: '#6DB33F' },
+  { name: 'NodeJS', icon: <SiNodedotjs color="#3C873A" />, color: '#3C873A' },
+  { name: 'Express', icon: <SiExpress color="#000000" />, color: '#000000' },
+  { name: 'SQL', icon: <SiPostgresql color="#336791" />, color: '#336791' },
+  { name: 'MongoDB', icon: <SiMongodb color="#4DB33D" />, color: '#4DB33D' },
   { name: 'Git', icon: <SiGit color="#f1502f" />, color: '#f1502f' },
   { name: 'GitHub', icon: <SiGithub color="#d5d4d4" />, color: '#d5d4d4' },
   { name: 'Jest', icon: <SiJest color="#18df16" />, color: '#18df16' },
-  { name: 'EJS', icon: '<%', color: '#b4ca65 ' },
-  { name: 'Vite', icon: <SiVite color="#646cff" />, color: '#646cff' },
-  { name: 'Passport', icon: <SiPassport color="#35df79" />, color: '#35df79' },
+  { name: 'JUnit', icon: <SiJunit5 color="#25A162" />, color: '#25A162' },
   { name: 'JWT', icon: <SiJsonwebtokens color="#d63aff" />, color: '#d63aff' },
   { name: 'Linux', icon: <SiLinux color="#d5d4d4" />, color: '#d5d4d4' },
 ];
